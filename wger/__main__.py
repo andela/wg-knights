59eac26f596ac0dabad3ab9e32ca44b4d579e362
James Kariuki <karyorkir@gmail.com>
2018-02-15 12:04:24 +0300

    [Chore #154953752] refactor code to meet flake8 standards

--- wger/__main__.py
+++ wger/__main__.py
@@ -16,7 +16,6 @@
 
 import sys
 from invoke import run
-
 '''
 This simple wrapper script is used as a console entry point in the packaged
 version of the application. It simply redirects all arguments to the invoke
--- wger/utils/cache.py
+++ wger/utils/cache.py
@@ -18,7 +18,6 @@ import hashlib
 from django.core.cache import cache
 from django.utils.encoding import force_bytes
 
-
 logger = logging.getLogger(__name__)
 
 
@@ -116,10 +115,5 @@ class CacheKeyMapper(object):
         '''
         return self.WORKOUT_LOG_LIST.format(hash_value)
 
-    def get_nutrition_plan_key(self, param):
-        '''
-        Return nutrition plan cache key
-        '''
-        return self.NUTRITION_PLAN_CACHE_KEY.format(self.get_pk(param))
 
 cache_mapper = CacheKeyMapper()
